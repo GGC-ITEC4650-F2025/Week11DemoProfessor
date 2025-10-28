7f5f4931a259afecdc489818721981575e183cd8
Sebastien Siva <ssiva@ggc.edu>
2025-10-28 17:06:35 -0400

    Quiz Solution For Finding Top Face of Die

--- Prof_DiceRoller/Assets/Scripts/DieController.cs
+++ Prof_DiceRoller/Assets/Scripts/DieController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class DieController : MonoBehaviour
 {
@@ -30,12 +31,19 @@ public class DieController : MonoBehaviour
         }
 
         //check all children
+
         //find the index of the one with the highest position.y
+        int best_i = 0;
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform child = transform.GetChild(i);
-            //print(child.name);
+            Transform bestChild = transform.GetChild(best_i);
+            if (child.position.y > bestChild.position.y)
+            {
+                best_i = i;
+            }
         }
         //set the currentValue to the highest child's name
+        currentValue = transform.GetChild(best_i).name;
     }
 }
